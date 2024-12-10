77d5b8063cd4884aaa5504b4b97b39a30ebf054c
SofianeSD <sofianesd51@gmail.com>
2024-12-10 17:39:25 +0100

    init API interfaces and adding API routes to access links. Adding bcrypt for password protection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
 
+let nanoid;
+(async () => {
+  const { nanoid: importedNanoid } = await import('nanoid');
+  nanoid = importedNanoid;
+})();
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect('mongodb://mongo:27017/pastebin', {
   useNewUrlParser: true,
@@ -19,6 +30,25 @@ const PasteSchema = new mongoose.Schema({
 });
 
 const Paste = mongoose.model('Paste', PasteSchema);
+
+app.post('/api/paste', async (req, res) => {
+  const { text, password } = req.body;
+  const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
+  const link = nanoid(10);
+  const newPaste = new Paste({ text, link, password: hashedPassword });
+  await newPaste.save();
+  res.json({ link: `http://localhost:4000/${link}` });
+});
+
+app.get('/:link', async (req, res) => {
+  const { link } = req.params;
+  const paste = await Paste.findOne({ link });
+  if (paste) {
+    res.json(paste);
+  } else {
+    res.status(404).json({ error: 'Paste non trouvé' });
+  }
+});
 
 
 app.listen(PORT, () => {
